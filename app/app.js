d7cbeb250c1b12f8097b4a5c7a5b85260b621818
Wallace Breza <wallace@breza.me>
2014-10-11 21:31:27 -0700

    Starting to set up gh-pages

---
+++ app/app.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app', ['dynamic-forms'])
+        .config(['$routeProvider', AppConfig]);
+
+    function AppConfig($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/app/views/home.html',
+                controller: 'homeController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+}());
---
+++ app/controllers/homeController.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app')
+        .controller('homeController', ['$scope', HomeController]);
+
+    function HomeController($scope) {
+        var a = 1;
+    }
+}());
